5d5a5a8ed2a0621f23583f0deb460c9f1303ad5b
Pragati Jaiswal <pragatijaiswal3132@gmail.com>
2025-10-17 00:14:52 +0530

    chore(env): update environment variables and authentication keys (except razor pay and Gemini)

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -1,16 +1,23 @@
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "loginlms-a7ea1.firebaseapp.com",
-  projectId: "loginlms-a7ea1",
-  storageBucket: "loginlms-a7ea1.firebasestorage.app",
-  messagingSenderId: "665916718747",
-  appId: "1:665916718747:web:16dbe0bfe5aeeface0903e"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "digividya-cc8a3.firebaseapp.com",
+  projectId: "digividya-cc8a3",
+  storageBucket: "digividya-cc8a3.firebasestorage.app",
+  messagingSenderId: "150756547489",
+  appId: "1:150756547489:web:ac1768a3e2e709e51d09e0"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app)
 const provider = new GoogleAuthProvider()
-export {auth,provider}
+
+export {auth, provider}
